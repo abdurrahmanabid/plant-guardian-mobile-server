e57bcfe2c147006afd8f41a498a6f4717222fb05
abdurrahmanabid <abdurrahmanabid33@gmail.com>
2025-07-23 02:00:11 +0600

    upload avatar route added

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import { signUpController } from "../controller/signUpController";
 import { signInController } from "../controller/signInController";
 import { isLoggedIn } from "../middleware/isLoggedIn";
+import uploadHandler from '@abdurrahmanabid/multi-file-upload';
+
 
 const router = Router()
 
 //routes
 router.post('/signup',signUpController) // api/user/signup
 router.post('/signin',isLoggedIn,signInController) // api/user/signin
+router.post("/upload-avatar", uploadHandler("single", [], "uploads/avatars"));
+
 
 export default router
