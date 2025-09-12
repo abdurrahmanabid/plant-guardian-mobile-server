fe833d88f3755f8a8ff35517d6392f01e0d96d53
abdurrahmanabid <abdurrahmanabid33@gmail.com>
2025-09-12 11:54:56 +0600

    /predict-fertilizer-and-treatment  added for  fertilizer and disease treatment suggestion

--- src/router/PredictedModelRouter.ts
+++ src/router/PredictedModelRouter.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import uploadHandler from "@abdurrahmanabid/multi-file-upload";
 import { diseasePredictionController } from "../controller/diseasePredictionController";
-
+import { fertilizerAndTreatmentPredictionController } from "../controller/fertilizerAndTreatmentPrediction";
 
 const router = Router();
 
 //routes
-router.post('/image-predict',diseasePredictionController)
+router.post("/image-predict", diseasePredictionController);
+router.post(
+  "/predict-fertilizer-and-treatment",
+  fertilizerAndTreatmentPredictionController
+);
 router.post(
   "/leaf-upload",
-  uploadHandler("single", ["image/jpeg", "image/png"], "uploads/leaf"),
+  uploadHandler("single", ["image/jpeg", "image/png"], "uploads/leaf")
 ); // api/predict/leaf-upload
 export default router;
